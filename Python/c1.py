ea55318a3c528f2b35b5a3107a519c18fc3df1e2
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-03-03 09:18:42 -0800

    Mom teaching me classes

--- Python/c1.py
+++ Python/c1.py
@@ -1,2 +1,21 @@
-class car:
+class student:
     
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f'''
+        Hello My name is {self.name}!!
+        I am {self.age} years old
+        How are you?
+        ''')
+
+    def GetIntroduction(self):
+        return f'''
+        Hello My name is {self.name}!!
+        I am {self.age} years old
+        How are you?
+        '''
+
+
